e8cec03c477dcefe81c43bd14f945d5bac884665
beomun <sos891214@gmail.com>
2018-10-01 23:30:54 +0900

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <router-link to="/">Go to Main</router-link>
+    <router-link to="/posts">Go to Posts</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Posts from './components/Posts.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Posts
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
